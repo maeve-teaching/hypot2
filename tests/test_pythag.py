17244afcd1d427ac67f75622d1d20fa4abd06d95
maeve-teaching <maeve.rse.teaching@gmail.com>
2024-10-14 11:34:52 +0000

    Add first tests

---
+++ tests/test_pythag.py
@@ -0,0 +1,31 @@
+import numpy as np
+import hypot2.pythag
+
+def test_add_nums():
+    '''Tests the function that adds two numbers'''
+
+    # Arrange
+    test_variable_a = 3.5
+    test_variable_b = 4
+    expected_output = 7.5
+
+    # Act
+    output = hypot2.pythag.add_nums(test_variable_a, test_variable_b)
+
+    # Assert
+    assert output == expected_output
+
+
+def test_add_nums_arrays():
+    '''Tests the function that adds two arrays'''
+
+    # Arrange
+    test_array_a = np.array([2, 5.7, 8])
+    test_array_b = np.array([1.0, 1.0, 1.0])
+    expected_output = np.array([3, 6.7, 9])
+
+    # Act
+    output = hypot2.pythag.add_nums(test_array_a, test_array_b)
+
+    # Assert
+    assert np.allclose(expected_output, output)
