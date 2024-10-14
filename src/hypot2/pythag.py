e0965af6bd0207c53a4ba7974e2b95ece605f1a9
maeve-teaching <maeve.rse.teaching@gmail.com>
2024-10-14 10:34:22 +0000

    Initial setup of hypot2 project
    Setting up folder structure following
    software-dev.streamlit.app

---
+++ src/hypot2/pythag.py
@@ -0,0 +1,31 @@
+# Module to calculate the hypotenuse of a
+# right angled triangle
+
+# Function to check input is float/integer
+# Throw error if not
+
+# Function to add two numbers together
+# input: floats or int or list/array of ints/floats
+def add_nums(a, b):
+    """Add two numbers together
+
+    Args:
+        a (float, int, array): A number to add
+        b (float, int, array): A number to add
+
+    Returns:
+        float, int, array: The sum of a and b
+    """
+    sum_of_numbers = a + b
+    return sum_of_numbers
+
+# Function to calculate the square of a number
+# input: floats or int or list/array of ints/floats
+
+# Function to calculate the square root of a number
+# input: floats or int or list/array of ints/floats
+
+# Function to calculate the hypotenuse using all the above
+# a^2 + b^2 = c^2
+# c = sqrt(a^2 + b^2)
+# input: floats or int or list/array of ints/floats
